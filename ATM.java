1f87710397194da47a1b9da77c311bd56dd3c495
somnathkundu <kundusomnath610@gmail.com>
2023-12-03 03:53:55 +0530

    Atm Machine Code Push...

---
+++ ATM.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class ATM {
+
+    float Balance;
+    int pin = 5678;
+
+    public void checkPin() {
+        System.out.println("Enter Your pin:");
+        Scanner sc = new Scanner(System.in);
+        int enteredPin = sc.nextInt();
+        if (enteredPin == pin) {
+            menu();
+        }
+        else {
+            System.out.println("Entered pin Wrong");
+        }
+    }
+
+    public void menu() {
+        System.out.println("Enter your choice");
+        System.out.println("1. Check your Bal");
+        System.out.println("2. Withdraw Money");
+        System.out.println("3. Deposit Money");
+        System.out.println("4. Exit");
+
+        Scanner scn = new Scanner(System.in);
+        int opt = scn.nextInt();
+
+        if (opt == 1) {
+            checkBal();
+        } else if (opt == 2) {
+            withdraw();
+        } else if (opt == 3) {
+            DepositMoney();
+        } else if (opt == 4) {
+            Exit();
+        } else {
+            System.out.println("Enter a valid input");
+        }
+    }
+
+    public void checkBal() {
+        System.out.println("Print Balance:-" + Balance);
+        menu();
+    }
+
+    public void withdraw() {
+        System.out.println("Enter the amount");
+        Scanner scn = new Scanner(System.in);
+        float amount = scn.nextFloat();
+        if(amount > Balance) {
+            System.out.println("Insufficient bal");
+        } else {
+            Balance = Balance - amount;
+            System.out.println("Money withdrawn Successfully");
+            menu();
+        }
+    }
+
+    public void DepositMoney() {
+        System.out.println("Enter The amount");
+        Scanner sc = new Scanner(System.in);
+        float amount = sc.nextFloat();
+        Balance = Balance + amount;
+        System.out.println("Money Deposit Successful");
+        menu();
+    }
+
+    public void Exit() {
+        System.out.println("Thank You!!!");
+    }
+}
---
+++ ATMMachine.java
@@ -0,0 +1,8 @@
+
+public class ATMMachine {
+    public static  void main(String [] args) {
+
+        ATM obj = new ATM();
+        obj.checkPin();
+    }
+}
